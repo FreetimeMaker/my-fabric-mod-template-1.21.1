e7ddf9b9bbe28fb6929ed740466dbcd0e8f3fb19
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2025-02-16 20:14:37 +0100

    a&c

---
+++ src/main/java/com/freetime/MyFabricModClient.java
@@ -0,0 +1,10 @@
+package com.freetime;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFabricModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/freetime/item/ModItems.java
@@ -0,0 +1,26 @@
+package com.freetime.item;
+
+import com.freetime.MyFabricMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(MyFabricMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        MyFabricMod.LOGGER.info("Registering Mod Items for " + MyFabricMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+        } );
+    }
+}
