6370c878bb0353ce2df4a8f4cd826cb7d183df4b
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2025-02-16 21:02:21 +0100

    a&c

--- src/main/java/com/freetime/MyFabricMod.java
+++ src/main/java/com/freetime/MyFabricMod.java
@@ -1,5 +1,6 @@
 package com.freetime;
 
+import com.freetime.block.ModBlocks;
 import com.freetime.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,5 +14,6 @@ public class MyFabricMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
