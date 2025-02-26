51778f3ebb519de69b0b5eb288457572e7c70cb6
Freetime Maker <jamieachatz@gmail.com>
2025-02-26 14:43:38 +0100

    Add status effect to GOLDEN_BEEF_RAW item.
    Added a Hunger status effect to the GOLDEN_BEEF_RAW food component with a low probability. This change enhances the item's mechanics, adding balance and risk when consuming it.

--- src/main/java/com/freetime/item/ModFoodComponents.java
+++ src/main/java/com/freetime/item/ModFoodComponents.java
@@ -8,6 +8,7 @@ public class ModFoodComponents {
     public static final FoodComponent GOLDEN_BEEF_COOKED = new FoodComponent.Builder().nutrition(3).saturationModifier(0.25f)
             .statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 200), 0.15f).build();
 
-    public static final FoodComponent GOLDEN_BEEF_RAW = new FoodComponent.Builder().nutrition(2).saturationModifier(0.25f).build();
+    public static final FoodComponent GOLDEN_BEEF_RAW = new FoodComponent.Builder().nutrition(2).saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 200), 0.15f).build();
 
 }
