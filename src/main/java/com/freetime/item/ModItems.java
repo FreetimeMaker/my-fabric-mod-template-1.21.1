98a301ffcddec407c1a830e5964d6d059db5c713
Freetime Maker <jamieachatz@gmail.com>
2025-02-26 14:23:51 +0100

    Add loot tables and minor fixes for gold-related items
    Introduced loot tables for gold ore, deepslate gold ore, golden blocks, and raw golden blocks. Removed unused golden beef items and fixed a typo in the raw golden block item model. Updated `fabric.mod.json` with additional metadata and dependency version adjustments.

--- src/main/java/com/freetime/item/ModItems.java
+++ src/main/java/com/freetime/item/ModItems.java
@@ -9,8 +9,6 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item GOLDEN_BEEF_COOKED = registerItem("golden_beef_cooked", new Item(new Item.Settings()));
-    public static final Item GOLDEN_BEEF_RAW = registerItem("golden_beef_raw", new Item(new Item.Settings()));
     public static final Item RAW_GOLDEN_INGOT = registerItem("raw_golden_ingot", new Item(new Item.Settings()));
     public static final Item GOLDEN_INGOT = registerItem("golden_ingot", new Item(new Item.Settings()));
     public static final Item GOLDEN_GROUP_ICON = registerItem("golden_group_icon", new Item(new Item.Settings()));
